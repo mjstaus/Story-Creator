38bc7177bc1d24f8fb9a2564e9aea062523b246c
Martha Staus <mjstaus@gmail.com>
2022-02-01 10:23:25 -0800

    Fix body-parser deprication issue

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ db.connect();
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
 app.use(morgan("dev"));
-app.use(bodyParser);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -40,15 +42,11 @@ app.use(express.static("public"));
 const usersRoutes = require("./routes/users");
 const widgetsRoutes = require("./routes/widgets");
 
-const storiesRoutes = require("./routes/stories")
-const contributionsRoutes = require("./routes/contributions")
-
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
 app.use("/api/users", usersRoutes(db));
 app.use("/api/widgets", widgetsRoutes(db));
-app.use("/stories", storiesRoutes(db));
-app.use("/stories/:id/contributions", contributionsRoutes(db));
+
 // Note: mount other resources here, using the same pattern above
 
 // Home page
