24db23d2665d522e0612b329ff948f938bcab6fc
Martha Staus <mjstaus@gmail.com>
2022-02-02 20:49:06 -0800

    connect /stories route to db

--- routes/stories.js
+++ routes/stories.js
@@ -1,65 +1,55 @@
-const express = require('express');
-const router  = express.Router();
-
-// module.exports = (db) => {
-//   router.get("/", (req, res) => {
-//     db.query(`SELECT * FROM stories;`)
-//       .then(data => {
-//         const stories = data.rows;
-//         res.json({ stories });
-//       })
-//       .catch(err => {
-//         res
-//           .status(500)
-//           .json({ error: err.message });
-//       });
-//   });
-//   router.post()
-//   return router;
-// };
+const express = require("express");
+const { renderSync } = require("sass");
+const router = express.Router();
 
 module.exports = (db) => {
-
-//All stories
   router.get("/", (req, res) => {
-    res.render("stories/stories_index")
+    db.query(`SELECT * FROM stories;`)
+      .then((data) => {
+        const templateVars = { stories: data.rows };
+        res.render("stories/stories_index", templateVars);
+      })
+      .catch((err) => {
+        res.status(500).json({ error: err.message });
+      });
   });
 
   router.get("/:id", (req, res) => {
-    res.send('View one story')
+    res.send("View one story");
   });
 
   router.get("/:id/contributions", (req, res) => {
-    res.send('View all contributions for a story')
+    res.send("View all contributions for a story");
   });
 
   router.get("/:id/contributions/:id", (req, res) => {
-    res.send('View one contribution')
+    res.send("View one contribution");
   });
 
   router.post("/", (req, res) => {
-    res.send('Create new story')
+    res.send("Create new story");
   });
 
   router.post("/:id", (req, res) => {
-    res.send('Edit story')
+    res.send("Edit story");
   });
 
   router.post("/:id/contributions", (req, res) => {
-    res.send('Create new contribution')
+    res.send("Create new contribution");
   });
 
   router.post("/:id/contributions/:id", (req, res) => {
-    res.send('Edit contribution')
+    res.send("Edit contribution");
   });
 
   router.post("/:id/delete", (req, res) => {
-    res.send('Delete story')
+    res.send("Delete story");
   });
 
   router.post("/:id/contributions/:id/delete", (req, res) => {
-    res.send('Edit story status')
+    res.send("Edit story status");
   });
 
+  // router.post()
   return router;
-}
+};
