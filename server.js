49d7cd3179596f5e32557dbaebfbc475c5deb08a
Martha Staus <mjstaus@gmail.com>
2022-02-02 20:49:25 -0800

    connect db to router

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ const storiesRoutes = require("./routes/stories");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
+app.use("/users", usersRoutes(db));
 app.use("/stories", storiesRoutes(db));
 
 // Note: mount other resources here, using the same pattern above
