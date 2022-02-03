64303cf48f1e59caecd62883cc1a3733b4bac841
Amy <amymccarthy2014@gmail.com>
2022-02-02 17:33:50 -0800

    add archive column to contributions table

--- db/schema/schema.sql
+++ db/schema/schema.sql
@@ -13,6 +13,7 @@ CREATE TABLE users (
 CREATE TABLE stories (
   id SERIAL PRIMARY KEY NOT NULL,
   creator_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- Do we want the stories to be deleted if creator deletes account?
+  name VARCHAR(255) NOT NULL,
   initial_content TEXT,
   complete BOOLEAN NOT NULL DEFAULT FALSE
 );
@@ -22,7 +23,8 @@ CREATE TABLE contributions (
   contributor_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
   story_id INTEGER NOT NULL REFERENCES stories(id) ON DELETE CASCADE,
   content TEXT,
-  accepted BOOLEAN NOT NULL DEFAULT FALSE
+  accepted BOOLEAN NOT NULL DEFAULT FALSE,
+  archived BOOLEAN NOT NULL DEFAULT FALSE
 );
 
 CREATE TABLE contribution_votes (
